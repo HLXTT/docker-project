7155391394e7834e40d1c009a41b732c8334c992
Dang-Mai-Hoang-Long <dangmaihoanglong17012005@gmail.com>
2025-03-30 10:45:11 +0700

    sửa lại file connect.php

--- src/connect.php
+++ src/connect.php
@@ -1,11 +1,12 @@
 <?php
 
-    $server = 'mysql.railway.internal';
+    $server = 'switchback.proxy.rlwy.net';
+    $port = '13633';
     $user = 'root';
     $pass = 'LNNDnPHPdoqCZHZbudFhiDqEbVneScdF';
     $database = 'railway';
 
-    $conn = new mysqli($server, $user, $pass, $database);
+    $conn = new mysqli($server, $user, $pass, $database, $port);
 
     if ($conn) {
         mysqli_query($conn, 'SET NAMES "utf8"');
