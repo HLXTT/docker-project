a5f81a3773397b70dda8b8f0d80cae9d36c9f136
Dang-Mai-Hoang-Long <dangmaihoanglong17012005@gmail.com>
2025-04-01 00:17:15 +0700

    new

--- src/header.php
+++ src/header.php
@@ -19,7 +19,7 @@
         <div class="header-left">
             <!-- Logo thương hiệu -->
             <div class="logo">
-                <a href="index.php"><img src="https://docker-project-image-server-production.up.railway.app/images/logo.jpg" alt="logo" class="logo-img"></a>
+                <a href="index.php"><img src="/usr/share/nginx/html/images/logo.jpg" alt="logo" class="logo-img"></a>
             </div>
         </div>
 
