5815c1dde73e3a7888b7e6e178fc196b1a5d7ba0
Dang-Mai-Hoang-Long <dangmaihoanglong17012005@gmail.com>
2025-03-31 20:41:46 +0700

    new

--- src/add_product.php
+++ src/add_product.php
@@ -28,9 +28,10 @@
         $uploadDir = '/usr/share/nginx/html/images/product/';
 
 // Kiểm tra và tạo thư mục nếu chưa có
+        echo ini_get('open_basedir');
         if (!file_exists($uploadDir)) {
             if (!mkdir($uploadDir, 0775, true)) {
-                echo ini_get('open_basedir');
+                
                 die("Lỗi: Không thể tạo thư mục upload!");
             }
             // Sử dụng quyền root để thay đổi owner
