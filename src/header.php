066e54f5fca4174dfa4ac1cfdf5d8d207102f017
Dang-Mai-Hoang-Long <dangmaihoanglong17012005@gmail.com>
2025-04-05 14:08:42 +0700

    new

--- src/header.php
+++ src/header.php
@@ -8,8 +8,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hyper Tech</title>
-    <link rel="stylesheet" href="/Style.css">
-    <link rel="stylesheet" href="/product.css">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="product-info.css">
+    <link rel="stylesheet" href="allproduct.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
 </head>
 <body>
